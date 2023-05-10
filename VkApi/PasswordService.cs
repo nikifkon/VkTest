9ee4e50d13879700aec0eade9ee7ab6a33986fee
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2023-05-10 20:45:06 +0500

    Implement password service

--- VkApi/PasswordService.cs
+++ VkApi/PasswordService.cs
@@ -1,14 +1,38 @@
-﻿namespace VkApi;
+﻿using System.Security.Cryptography;
+using System.Text;
 
+namespace VkApi;
+
+// Adapted from https://code-maze.com/csharp-hashing-salting-passwords-best-practices/
 public class PasswordService : IPasswordService
 {
+    private const int KeySize = 64;
+    private const int Iterations = 100_000;
+    private readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
+    
     public string Encrypt(string password)
     {
-        throw new NotImplementedException();
+        var salt = RandomNumberGenerator.GetBytes(KeySize);
+        var hash = HashWithSalt(password, salt);
+        return $"{Convert.ToBase64String(salt)};{Convert.ToBase64String(hash)}";
     }
 
     public bool Verify(string password, string encrypted)
     {
-        throw new NotImplementedException();
+        var tokens = encrypted.Split(';');
+        var salt = Convert.FromBase64String(tokens[0]);
+        var hash = Convert.FromBase64String(tokens[1]);
+        var actualHash = HashWithSalt(password, salt);
+        return hash.SequenceEqual(actualHash);
+    }
+
+    private byte[] HashWithSalt(string password, byte[] salt)
+    {
+        return Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(password),
+            salt,
+            Iterations,
+            _hashAlgorithm,
+            KeySize);
     }
 }
--- VkApi/Program.cs
+++ VkApi/Program.cs
@@ -1,12 +1,14 @@
+using VkApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("VkApiDb");
 builder.Services.AddNpgsql<UserContext>(connectionString);
-
+builder.Services.AddSingleton<IPasswordService, PasswordService>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!!!");
+app.MapGet("/", (IPasswordService ps) => ps.Encrypt("Hello, World!"));
 
 app.Run();
 
