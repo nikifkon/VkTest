19ed2bc2653c92a7cd14688fc6d22b8cbc369696
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2023-05-10 22:33:50 +0500

    fix tests

--- VkApi.Tests/UserApiTest.cs
+++ VkApi.Tests/UserApiTest.cs
@@ -1,102 +1,122 @@
+using System.Data.Common;
 using System.Net;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 namespace VkApiTests;
 
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<UserContext>));
+
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbConnection));
+
+            services.Remove(dbConnectionDescriptor);
+
+            // Create open SqliteConnection so EF won't automatically close it.
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+                
+                return connection;
+            });
+
+            services.AddDbContext<UserContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+        
+        builder.UseEnvironment("Development");
+    }
+}
+
 public class TestDatabaseFixture
 {
-    private const string ConnectionString = @"host=localhost;port=5431;database=vktest;username=vktest;password=vktest";
-
-    private static readonly object _lock = new();
     public readonly Dictionary<GroupCode, UserGroup> Groups = new();
     public readonly Dictionary<StateCode, UserState> States = new();
     public readonly List<User> Users = new();
-    private static bool _databaseInitialized;
 
-    public TestDatabaseFixture()
+    public TestDatabaseFixture(UserContext context)
     {
-        lock (_lock)
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        Groups[GroupCode.Admin] = context.UserGroups.First(group => group.Code == GroupCode.Admin);
+        Groups[GroupCode.User] = context.UserGroups.First(group => group.Code == GroupCode.User);
+        States[StateCode.Active] = context.UserStates.First(state => state.Code == StateCode.Active);
+        States[StateCode.Blocked] = context.UserStates.First(state => state.Code == StateCode.Blocked);
+        Users = new()
         {
-            if (_databaseInitialized) return;
-            using var context = CreateContext();
-
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            Groups[GroupCode.Admin] = new UserGroup() { Code = GroupCode.Admin, Description = "This is admin user group" };
-            Groups[GroupCode.User] = new UserGroup() { Code = GroupCode.User, Description = "" };
-            States[StateCode.Active] = new UserState() { Code = StateCode.Active, Description = "Active user" };
-            States[StateCode.Blocked] = new UserState() { Code = StateCode.Blocked, Description = "Deleted user" };
-            Users = new()
+            new User
             {
-                new User
-                {
-                    Id = 1,
-                    Login = "First",
-                    Password = "secret1",
-                    CreatedDate = new DateOnly(2023, 5, 9),
-                    Group = Groups[GroupCode.Admin],
-                    State = States[StateCode.Active]
-                },
-                new User
-                {
-                    Id = 2,
-                    Login = "Second",
-                    Password = "secret2",
-                    CreatedDate = new DateOnly(2023, 5, 10),
-                    Group = Groups[GroupCode.User],
-                    State = States[StateCode.Active]
-                },
-                new User
-                {
-                    Id = 3,
-                    Login = "Bloced",
-                    Password = "secret3",
-                    CreatedDate = new DateOnly(2023, 5, 8),
-                    Group = Groups[GroupCode.User],
-                    State = States[StateCode.Blocked]
-                }
-            };
-            context.AddRange(
-                Groups[GroupCode.Admin],
-                Groups[GroupCode.User],
-                States[StateCode.Active],
-                States[StateCode.Blocked]
-            );
-            context.AddRange(Users);
-            context.SaveChanges();
+                Id = 1,
+                Login = "First",
+                Password = "secret1",
+                CreatedDate = new DateOnly(2023, 5, 9),
+                Group = Groups[GroupCode.Admin],
+                State = States[StateCode.Active]
+            },
+            new User
+            {
+                Id = 2,
+                Login = "Second",
+                Password = "secret2",
+                CreatedDate = new DateOnly(2023, 5, 10),
+                Group = Groups[GroupCode.User],
+                State = States[StateCode.Active]
+            },
+            new User
+            {
+                Id = 3,
+                Login = "Bloced",
+                Password = "secret3",
+                CreatedDate = new DateOnly(2023, 5, 8),
+                Group = Groups[GroupCode.User],
+                State = States[StateCode.Blocked]
+            }
+        };
+        context.AddRange(Users);
+        context.SaveChanges();
 
-            _databaseInitialized = true;
-        }
     }
-
-    private UserContext CreateContext()
-        => new (new DbContextOptionsBuilder<UserContext>()
-                .UseNpgsql(ConnectionString)
-                .Options);
 }
 
-public class ErrorMsg
+public class UnitTest1
 {
-    public string Message { get; set; }
-}
-
-public class UnitTest1 : IClassFixture<TestDatabaseFixture>
-{
-    private TestDatabaseFixture _dbFixture;
-    
-    public UnitTest1(TestDatabaseFixture dbFixture)
+    async private Task<(HttpClient client, TestDatabaseFixture dbFixture)> SetupClient()
     {
-        _dbFixture = dbFixture;
+        var application = new CustomWebApplicationFactory<Program>();
+        var scopeFactory = application.Services.GetService<IServiceScopeFactory>();
+        using var scope = scopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<UserContext>();
+        var _dbFixture = new TestDatabaseFixture(context);
+        var client = application.CreateClient();
+        return (client, _dbFixture);
     }
-
+    
     [Fact]
     public async Task TestCreateEndpoint()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
+        var (client, dbFixture) = await SetupClient();
         var user = new User { Login = "new user", Password = "secret1" };
         
         // Act
@@ -104,11 +124,11 @@ public class UnitTest1 : IClassFixture<TestDatabaseFixture>
         
         // Assert
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-
+    
         var createdUser = await response.Content.ReadFromJsonAsync<User>();
         Assert.NotNull(createdUser);
-        Assert.Equal(_dbFixture.States[StateCode.Active], createdUser.State);
-        Assert.Equal(_dbFixture.Groups[GroupCode.User], createdUser.Group);
+        Assert.Equivalent(dbFixture.States[StateCode.Active], createdUser.State);
+        Assert.Equivalent(dbFixture.Groups[GroupCode.User], createdUser.Group);
         Assert.Equal(DateOnly.FromDateTime(DateTime.Today), createdUser.CreatedDate);
         Assert.Equal(user.Login, createdUser.Login);
     }
@@ -117,56 +137,50 @@ public class UnitTest1 : IClassFixture<TestDatabaseFixture>
     public async Task TestGetEndpoint()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
-        var userInDb = _dbFixture.Users[0];
+        var (client, dbFixture) = await SetupClient();
+        var userInDb = dbFixture.Users[0];
         
         // Act
-        var response = await client.GetAsync($"/user/${userInDb.Login}");
+        var response = await client.GetAsync($"/user/{userInDb.Login}");
         
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
+    
         var user = await response.Content.ReadFromJsonAsync<User>();
-        Assert.Equal(userInDb, user);
+        Assert.Equivalent(userInDb, user);
     }
-
+    
     [Fact]
     public async Task TestCreateEndpoint_AlreadyExists()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
-        var user = new User { Login =_dbFixture.Users[0].Login, Password = "123" };
+        var (client, dbFixture) = await SetupClient();
+        var user = new User { Login = dbFixture.Users[0].Login, Password = "123" };
         
         // Act
         var response = await client.PostAsJsonAsync("/user", user);
         
         // Assert
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-
-        var errorMsg = await response.Content.ReadFromJsonAsync<ErrorMsg>();
-        Assert.NotNull(errorMsg);
-        Assert.Equal("Login has already taken", errorMsg.Message);
+        var errorMsg = await response.Content.ReadAsStringAsync();
+        Assert.Equal("\"Login has already taken\"", errorMsg);
     }
-
+    
     [Fact]
     public async Task TestCreateEndpoint_SingleAdmin()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
-        var user = new User { Login = "Second admin", Password = "123", Group = _dbFixture.Groups[GroupCode.Admin]};
+        var (client, dbFixture) = await SetupClient();
+        var user = new User { Login = "Second admin", Password = "123", Group = dbFixture.Groups[GroupCode.Admin]};
         
         // Act
         var response = await client.PostAsJsonAsync("/user", user);
         
         // Assert
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-
-        var errorMsg = await response.Content.ReadFromJsonAsync<ErrorMsg>();
-        Assert.NotNull(errorMsg);
-        Assert.Equal("Admin has already created and must be single", errorMsg.Message);
+    
+        var errorMsg = await response.Content.ReadAsStringAsync();
+        Assert.Equal("\"Admin has already created and must be single\"", errorMsg);
     }
     
     
@@ -174,18 +188,17 @@ public class UnitTest1 : IClassFixture<TestDatabaseFixture>
     public async Task TestListEndpoint()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
+        var (client, dbFixture) = await SetupClient();
         
         // Act
         var response = await client.GetAsync("/user");
         
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
+    
         var users = await response.Content.ReadFromJsonAsync<IList<User>>();
         Assert.NotNull(users);
-        _dbFixture.Users.Where(user => user.State.Code == StateCode.Active).AssertListEqual(users, user => user.Id);
+        dbFixture.Users.Where(user => user.State.Code == StateCode.Active).AssertListEqual(users, user => user.Id);
     }
     
         
@@ -193,13 +206,12 @@ public class UnitTest1 : IClassFixture<TestDatabaseFixture>
     public async Task TestDeleteEndpoint()
     {
         // Arrange
-        await using var application = new WebApplicationFactory<Program>();
-        using var client = application.CreateClient();
-        var userToDelete = _dbFixture.Users.First(user => user.State.Code != StateCode.Blocked);
+        var (client, dbFixture) = await SetupClient();
+        var userToDelete = dbFixture.Users.First(user => user.State.Code != StateCode.Blocked);
         
         // Act
-        var response = await client.DeleteAsync($"/user${userToDelete.Login}");
-        var getResponse = await client.GetAsync($"/user/${userToDelete.Login}");
+        var response = await client.DeleteAsync($"/user/{userToDelete.Login}");
+        var getResponse = await client.GetAsync($"/user/{userToDelete.Login}");
         
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
@@ -211,6 +223,6 @@ public static class AssertExt
 {
     public static void AssertListEqual<T>(this IEnumerable<T> expected, IEnumerable<T> actual, Func<T, int> keySelector)
     {
-        Assert.Equal(expected.OrderBy(keySelector), actual.OrderBy(keySelector));
+        Assert.Equivalent(expected.OrderBy(keySelector), actual.OrderBy(keySelector));
     }
 }
