def7be11756998be8563953941b85e2de5cc5400
Nikiforov Konstantin <kostya.nik.3854@gmail.com>
2023-05-11 10:20:37 +0500

    add swagger

--- VkApi/Program.cs
+++ VkApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using VkApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +8,19 @@ builder.Services.AddNpgsql<UserContext>(connectionString);
 builder.Services.AddSingleton<IPasswordService, PasswordService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VkTest API", Description = "Manage users", Version = "v1" });
+});
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
 app.MapUserApi();
 
 app.Run();
